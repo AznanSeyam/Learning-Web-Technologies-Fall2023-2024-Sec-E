d31ea6506a606a39518bf68f174e1282f9f8346b
AznanSeyam <aznan333@gmail.com>
2023-10-09 22:50:03 +0600

    final

---
+++ Lab_3_PHP/Question_A/gender/gender.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Gender</title>
+    </head>
+    <body>
+        <?php 
+        if(isset($_POST["submit"])) {
+            $gender = $_POST["gender"];
+            echo "Selected gender: " . $gender;
+        }
+        ?>
+    </body>
+</html>
